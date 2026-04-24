05337dd1d2ff7c9e8e269214612e95f9d03ad9f7
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-24 11:26:18 -0500

    fix deinit alloc before deinit res in test

--- src/test.zig
+++ src/test.zig
@@ -5,10 +5,9 @@ test "basic test" {
     const src:[]u8 = @constCast(@embedFile("test.bart"));
 
     var debug_alloc = std.heap.DebugAllocator(.{}).init;
+    defer _ = debug_alloc.deinit();
     const alloc = debug_alloc.allocator();
 
     var res = try bart.parse(alloc, src);
     defer res.deinit(alloc);
-
-    _ = debug_alloc.deinit();
 }
