3cd050883af648974be0bcd6a67b80976c328238
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-24 14:53:37 -0500

    test serialization

--- src/test.zig
+++ src/test.zig
@@ -12,8 +12,37 @@ test "parse test" {
 
     var res = try bart.parse(alloc, src);
     defer res.deinit(alloc);
+}
 
-    const serialized = try bart.serialize(alloc, &res);
+test "serialize test" {
+    const src = @constCast(
+        \\server {
+        \\  port = 8945;
+        \\  name = "foo";
+        \\  description = "foo bar baz";
+        \\  foo = [
+        \\    1
+        \\    9875
+        \\    "foo bar baz"
+        \\  ]
+        \\}
+    );
+
+    var debug_alloc = std.heap.DebugAllocator(.{}).init;
+    defer _ = debug_alloc.deinit();
+    const alloc = debug_alloc.allocator();
+
+    var res = try bart.parse(alloc, src);
+    defer res.deinit(alloc);
+
+    const serialized = try bart.serialize(alloc, &res, .{
+        .tab = .{
+            .char = .space,
+            .width = 2,
+        }
+    });
     defer alloc.free(serialized);
-    std.debug.print("{s}\n", .{serialized});
+    if (!std.mem.eql(u8, serialized, src)) {
+        try std.testing.expectEqualSlices(u8, src, serialized);
+    }
 }
